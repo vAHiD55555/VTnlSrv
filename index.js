d3f48eb676148d277f6446d05f86baebac364206
liMilCo <91998494+liMilCo@users.noreply.github.com>
2024-12-14 21:02:08 +0330

    Update index.js

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ export default async function handler(req, res) {
         //url.hostname = 'ictfz.ir';
         //url.protocol = 'https:';
 
-        const response = await fetch(url, {
+        const response = await fetch(url, {  //url.toString()
             method: req.method,
             headers: req.headers,
             body: req.body,
         });
 ////////////////////////////////////////    
-    res.status(200).send(Mytxt); //MyPage
+    //res.status(200).send(MyPage); //MyPage  
     //res.status(response.status).json(await response.text());
-    //res.status(response.status).send(await response.text());
+    res.status(response.status).send(await response.text());
     
     } catch (error) {
         res.status(500).json({ error: error.message });
