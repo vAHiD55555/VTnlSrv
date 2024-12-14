b9abf247ca13c4b95a3ab9fed18c02f82ef2f2a8
liMilCo <91998494+liMilCo@users.noreply.github.com>
2024-12-14 20:41:47 +0330

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ export default async function handler(req, res) {
             body: req.body,
         });
 ////////////////////////////////////////    
-    res.status(200).send(MyPage);
-    //res.status(response.status).send(await response.text());
+    //res.status(200).send(MyPage);
+    res.status(response.status).send(await response.text());
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
