4bf6a928fdf28caf3bbef987077bb3af906eee42
liMilCo <91998494+liMilCo@users.noreply.github.com>
2024-12-14 20:52:26 +0330

    Update index.js

--- index.js
+++ index.js
@@ -12,17 +12,19 @@ export default async function handler(req, res) {
      const MyURL = `https://host.chiphost.site${userID}`;   
 ////////////////////////////////
         const url = new URL(MyURL);
-
-
+        //url.hostname = 'ictfz.ir';
+        //url.protocol = 'https:';
+    const userPrt = url.protocol;
+const Mytxt = `prot: ${userPrt}`; 
         const response = await fetch(url.toString(), {
             method: req.method,
             headers: req.headers,
             body: req.body,
         });
 ////////////////////////////////////////    
-    //res.status(200).send(MyPage);
+    res.status(200).send(Mytxt); //MyPage
     //res.status(response.status).json(await response.text());
-    res.status(response.status).send(await response.text());
+    //res.status(response.status).send(await response.text());
     
     } catch (error) {
         res.status(500).json({ error: error.message });
