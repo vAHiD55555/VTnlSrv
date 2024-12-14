c52ba9ea6ab1f734cdbd4f931efc2baa55fff2da
liMilCo <91998494+liMilCo@users.noreply.github.com>
2024-12-14 20:13:43 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ export default async function handler(req, res) {
     const userID = req.url;
     const userHost = "my.com";
     const userTPass = "pass";
-    //${panelVersion}
+  
     const MyPage = `
      ID:  ${userID} 
      Host:  ${userHost} 
      TPass:  ${userTPass}   `;
-    
+////////////////////////////////
+        const url = new URL(req.url);
+        url.hostname = 'ictfz.ir';
+        url.protocol = 'https:';
+////////////////////////////////////////    
     res.status(200).send(MyPage);
     } catch (error) {
         res.status(500).json({ error: error.message });
